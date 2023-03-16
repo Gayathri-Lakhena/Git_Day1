508952cbb0742c09c292ae83ed341df7570a665f
Gayathri_Lakhena <gayatri.kalal2013@gmail.com>
2023-03-16 15:39:42 +0530

    Initial Commit

---
+++ Abc.java
@@ -0,0 +1,7 @@
+public class Abc
+{
+  public void m1()
+   {
+    System.out.println("m1 Developer 1");
+   }
+}
---
+++ Xyz.java
@@ -0,0 +1,7 @@
+public class Xyz
+{
+  public void m1()
+   {
+    System.out.println("m1 Developer 2");
+   }
+}
